db5d8c18ba985e0981cb47a8a28bc6fd8ac4f24e
vanhung16110109 <nvhungxx98@gmail.com>
2021-07-08 05:26:58 +0700

    - ver 1.9.6
    update

--- ecommerce/ecommerce/apps/product/views.py
+++ ecommerce/ecommerce/apps/product/views.py
@@ -10,43 +10,45 @@ import json
 
 
 def category_products(request, id, slug):
-    category = Category.objects.all()
-    products = Product.objects.filter(category_id = id)
-    current_user = request.user
-    shopcart = ShopCart.objects.filter(user_id=current_user.id)
-    total = 0
-    for rs in shopcart:
-        total += rs.variant.price * rs.quantity
-    quantity = 0
-    for rs in shopcart:
-        quantity += rs.quantity
-    context = {
-        'category':category,
-        'products': products,
+	category = Category.objects.all()
+	products = Product.objects.filter(category_id = id)
+	current_user = request.user
+	shopcart = ShopCart.objects.filter(user_id=current_user.id)
+	product_all = Product.objects.all()
+	total = 0
+	for rs in shopcart:
+		total += rs.variant.price * rs.quantity
+	quantity = 0
+	for rs in shopcart:
+		quantity += rs.quantity
+	context = {
+		'category':category,
+		'products': products,
 		'total': total,
-        'quantity': quantity,
-    }
-    return render(request, 'product/category_products.html',context)
+		'quantity': quantity,
+		'product_all': product_all,
+	}
+	return render(request, 'product/category_products.html',context)
 
 
 def addcomment(request, id):
-    url = request.META.get('HTTP_REFERER')
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            data = Comment()
-            data.subject = form.cleaned_data['subject']
-            data.comment = form.cleaned_data['comment']
-            data.rate = form.cleaned_data['rate']
-            data.ip = request.META.get('REMOTE_ADDR')
-            data.product_id = id
-            current_user = request.user
-            data.user_id = current_user.id
-            data.save()
-            messages.success(request, "Cảm ơn bạn đã gửi bình luận và đánh giá cho chúng tôi.")
-            return HttpResponseRedirect(url)
+	url = request.META.get('HTTP_REFERER')
+	if request.method == 'POST':
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			data = Comment()
+			data.subject = form.cleaned_data['subject']
+			data.comment = form.cleaned_data['comment']
+			data.rate = form.cleaned_data['rate']
+			data.ip = request.META.get('REMOTE_ADDR')
+			data.product_id = id
+			current_user = request.user
+			data.user_id = current_user.id
+			data.save()
+			messages.success(request, "Cảm ơn bạn đã gửi bình luận và đánh giá cho chúng tôi.")
+			return HttpResponseRedirect(url)
 
-    return HttpResponseRedirect(url)
+	return HttpResponseRedirect(url)
 
 
 def product_detail(request, id, slug):
@@ -65,14 +67,14 @@ def product_detail(request, id, slug):
 	comments = Comment.objects.filter(product_id=id)
 	# details = DetailsProduct.objects.filter(product_id=id)
 	context = {
-        'comments': comments,
-        'product': product,
-        'category': category,
-        'images': images,
+		'comments': comments,
+		'product': product,
+		'category': category,
+		'images': images,
 		'total': total,
-        'quantity': quantity,
+		'quantity': quantity,
 		# 'deta': details,
-    }
+	}
 	if product.variant !="None": # Product have variants
 		if request.method == 'POST': #if we select color
 			variant_id = request.POST.get('variantid')
@@ -86,46 +88,280 @@ def product_detail(request, id, slug):
 			sizes = Variants.objects.raw('SELECT * FROM  product_variants  WHERE product_id=%s GROUP BY size_id',[id])
 			variant = Variants.objects.get(id=variants[0].id)
 		context.update({'sizes': sizes, 'colors': colors,
-                        'variant': variant,'query': query
-                        })
+						'variant': variant,'query': query
+						})
 	return render(request, 'product/product-detail.html', context)
 
 
 def ajaxcolor(request):
-    data = {}
-    if request.POST.get('action') == 'post':
-        size_id = request.POST.get('size')
-        productid = request.POST.get('productid')
-        print(size_id)
-        print(productid)
-        colors = Variants.objects.filter(product_id=productid, size_id=size_id)
-        context = {
-            'size_id': size_id,
-            'productid': productid,
-            'colors': colors,
-        }
-        data = {'rendered_table': render_to_string('product/color_list.html', context=context)}
-        return JsonResponse(data)
-    return JsonResponse(data)
+	data = {}
+	if request.POST.get('action') == 'post':
+		size_id = request.POST.get('size')
+		productid = request.POST.get('productid')
+		print(size_id)
+		print(productid)
+		colors = Variants.objects.filter(product_id=productid, size_id=size_id)
+		context = {
+			'size_id': size_id,
+			'productid': productid,
+			'colors': colors,
+		}
+		data = {'rendered_table': render_to_string('product/color_list.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
 
 
 def category_products_pro_code(request, id, title):
-    category = Category.objects.all()
-    products = Product.objects.filter(pro_code = title)
-    current_user = request.user
-    shopcart = ShopCart.objects.filter(user_id=current_user.id)
-    product = Product.objects.all()
-    total = 0
-    for rs in shopcart:
-        total += rs.variant.price * rs.quantity
-    quantity = 0
-    for rs in shopcart:
-        quantity += rs.quantity
-    context = {
-        'category':category,
-        'products': products,
+	category = Category.objects.all()
+	products = Product.objects.filter(pro_code = title)
+	current_user = request.user
+	shopcart = ShopCart.objects.filter(user_id=current_user.id)
+	product = Product.objects.all()
+	total = 0
+	for rs in shopcart:
+		total += rs.variant.price * rs.quantity
+	quantity = 0
+	for rs in shopcart:
+		quantity += rs.quantity
+	filter_product_title = title
+	print(filter_product_title)
+	print(type(filter_product_title))
+	context = {
+		'category':category,
+		'products': products,
 		'total': total,
-        'quantity': quantity,
-        'product': product,
-    }
-    return render(request, 'product/category_products_pro_code.html',context)
+		'quantity': quantity,
+		'product': product,
+		'filter_product_title': filter_product_title
+	}
+	return render(request, 'product/category_products_pro_code.html',context)
+
+
+def ajax_manufacturer_allproduct(request):
+	data = {}
+	if request.POST.get('action') == 'post':
+		print(1)
+		AllProduct = request.POST.get('AllProduct')
+		print(AllProduct)
+		products = Product.objects.all()
+		# # alltotal = request.POST.get('alltotal')                      
+		total2 = request.POST.get('totalID')   
+		print(total2)                           
+		# # total4 = request.POST.get('total4')                            
+		# # total7 = request.POST.get('total7')                             
+		# # total13 = request.POST.get('total13')                              
+		# # big13 = request.POST.get('big13')
+		# # total = request.POST.get('total')
+		# # print(total)
+		# totalID = request.POST.get('totalID')
+		# if totalID == "alltotal":
+		# 	print(1)
+		# elif totalID == "2000000":
+		# 	print(2)
+		# elif totalID == 4000000:
+		# 	print(4)
+		# #print(alltotal, total2, total4, total7, total13, big13)
+		# #print(products[1])
+		# product = []
+		# for i in range(len(products)):
+		# 	product.append(products[i])
+		# print(product)
+		context = {
+			'products': products,
+		}
+		data = {'rendered_table': render_to_string('product/advanced-searchs.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
+
+
+def ajax_manufacturer_apple(request):
+	data = {}
+	if request.POST.get('action') == 'post':
+		print(1)
+		Apple = request.POST.get('Apple')
+		print(Apple)
+		products = Product.objects.filter(pro_code = Apple)	
+		context = {
+			'products': products,
+		}
+		data = {'rendered_table': render_to_string('product/advanced-searchs.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
+
+
+def ajax_manufacturer_samsung(request):
+	data = {}
+	if request.POST.get('action') == 'post':
+		print(1)
+		Samsung = request.POST.get('Samsung')	
+		products = Product.objects.filter(pro_code = Samsung)	
+		context = {
+			'products': products,
+		}
+		data = {'rendered_table': render_to_string('product/advanced-searchs.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
+
+
+def ajax_manufacturer_oppo(request):
+	data = {}
+	if request.POST.get('action') == 'post':
+		print(1)
+		Oppo = request.POST.get('Oppo')		
+		products = Product.objects.filter(pro_code = Oppo)
+		context = {
+			'products': products,
+		}
+		data = {'rendered_table': render_to_string('product/advanced-searchs.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
+
+
+def ajax_manufacturer_vivo(request):
+	data = {}
+	if request.POST.get('action') == 'post':
+		print(1)
+		Vivo = request.POST.get('Vivo')		
+		products = Product.objects.filter(pro_code = Vivo)
+		context = {
+			'products': products,
+		}
+		data = {'rendered_table': render_to_string('product/advanced-searchs.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
+
+
+def ajax_manufacturer_vsmart(request):
+	data = {}
+	if request.POST.get('action') == 'post':
+		print(1)
+		Vsmart = request.POST.get('Vsmart')		
+		products = Product.objects.filter(pro_code = Vsmart)
+		context = {
+			'products': products,
+		}
+		data = {'rendered_table': render_to_string('product/advanced-searchs.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
+
+
+def ajax_manufacturer_xiaomi(request):
+	data = {}
+	if request.POST.get('action') == 'post':
+		print(1)
+		Xiaomi = request.POST.get('Xiaomi')		
+		products = Product.objects.filter(pro_code = Xiaomi)
+		context = {
+			'products': products,
+		}
+		data = {'rendered_table': render_to_string('product/advanced-searchs.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
+
+
+def ajax_manufacturer_realme(request):
+	data = {}
+	if request.POST.get('action') == 'post':
+		print(1)
+		Realme = request.POST.get('Realme')
+		products = Product.objects.filter(pro_code = Realme)
+		context = {
+			'products': products,
+		}
+		data = {'rendered_table': render_to_string('product/advanced-searchs.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
+
+
+def ajax_manufacturer_masstel(request):
+	data = {}
+	if request.POST.get('action') == 'post':
+		print(1)
+		Masstel = request.POST.get('Masstel')		
+		products = Product.objects.filter(pro_code = Masstel)
+		context = {
+			'products': products,
+		}
+		data = {'rendered_table': render_to_string('product/advanced-searchs.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
+
+
+def ajax_manufacturer_huawei(request):
+	data = {}
+	if request.POST.get('action') == 'post':
+		print(1)
+		Huawei = request.POST.get('Huawei')	
+		products = Product.objects.filter(pro_code = Huawei)
+		context = {
+			'products': products,
+		}
+		data = {'rendered_table': render_to_string('product/advanced-searchs.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
+
+
+def ajax_manufacturer_nokia(request):
+	data = {}
+	if request.POST.get('action') == 'post':
+		print(1)
+		Nokia = request.POST.get('Nokia')		
+		products = Product.objects.filter(pro_code = Nokia)
+		context = {
+			'products': products,
+		}
+		data = {'rendered_table': render_to_string('product/advanced-searchs.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
+
+
+def ajax_manufacturer_totalall(request):
+	data = {}
+	if request.POST.get('action') == 'post':
+		print(1)
+		AllProduct = request.POST.get('AllProduct')
+		Apple = request.POST.get('Apple')
+		Samsung = request.POST.get('Samsung')
+		Oppo = request.POST.get('Oppo')
+		Vivo = request.POST.get('Vivo')
+		Vsmart = request.POST.get('Vsmart')
+		Xiaomi = request.POST.get('Xiaomi')
+		Realme = request.POST.get('Realme')
+		Masstel = request.POST.get('Masstel')
+		Huawei = request.POST.get('Huawei')
+		Nokia = request.POST.get('Nokia')
+		totalall = request.POST.get('totalall')
+		print(AllProduct, Apple,Samsung, Oppo, Vivo, Vsmart, Xiaomi, Realme, Masstel, Huawei, Nokia)
+		print(totalall)
+		context = {
+		}
+		data = {'rendered_table': render_to_string('product/advanced-searchs.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
+
+
+def ajax_manufacturer_total2(request):
+	data = {}
+	if request.POST.get('action') == 'post':
+		print(1)
+		AllProduct = request.POST.get('AllProduct')
+		Apple = request.POST.get('Apple')
+		Samsung = request.POST.get('Samsung')
+		Oppo = request.POST.get('Oppo')
+		Vivo = request.POST.get('Vivo')
+		Vsmart = request.POST.get('Vsmart')
+		Xiaomi = request.POST.get('Xiaomi')
+		Realme = request.POST.get('Realme')
+		Masstel = request.POST.get('Masstel')
+		Huawei = request.POST.get('Huawei')
+		Nokia = request.POST.get('Nokia')
+		total2 = request.POST.get('total2')
+		print(AllProduct, Apple,Samsung, Oppo, Vivo, Vsmart, Xiaomi, Realme, Masstel, Huawei, Nokia)
+		print(total2)
+		context = {
+		}
+		data = {'rendered_table': render_to_string('product/advanced-searchs.html', context=context)}
+		return JsonResponse(data)
+	return JsonResponse(data)
+
+
--- ecommerce/ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/ecommerce/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 from django.contrib import admin
 from apps.product.views import ajaxcolor
 from apps.vnlocation.views import ajaxAPIlocationdistrict, ajaxAPIlocationward, ajaxGHTK, ajaxGHN, ajaxGHN_online, ajaxGHTK_online
+from apps.product.views import ajax_manufacturer_allproduct, ajax_manufacturer_apple, ajax_manufacturer_samsung, ajax_manufacturer_oppo, ajax_manufacturer_vivo, ajax_manufacturer_vsmart, ajax_manufacturer_xiaomi, ajax_manufacturer_realme, ajax_manufacturer_masstel, ajax_manufacturer_huawei, ajax_manufacturer_nokia, ajax_manufacturer_totalall, ajax_manufacturer_total2
 from django.conf.urls import url
 import apps.vnpay_python.views
 from apps.product.views import category_products_pro_code
@@ -29,6 +30,20 @@ urlpatterns = [
     path('ajaxGHN/', ajaxGHN, name='ajaxGHN'),
     path('ajaxGHTK_online/', ajaxGHTK_online, name='ajaxGHTK_online'),
     path('ajaxGHN_online/', ajaxGHN_online, name='ajaxGHN_online'),
+	path('ajax_manufacturer_allproduct/', ajax_manufacturer_allproduct, name='ajax_manufacturer_allproduct'),
+	path('ajax_manufacturer_apple/', ajax_manufacturer_apple, name='ajax_manufacturer_apple'),
+	path('ajax_manufacturer_samsung/', ajax_manufacturer_samsung, name='ajax_manufacturer_samsung'),
+	path('ajax_manufacturer_oppo/', ajax_manufacturer_oppo, name='ajax_manufacturer_oppo'),
+	path('ajax_manufacturer_vivo/', ajax_manufacturer_vivo, name='ajax_manufacturer_vivo'),
+	path('ajax_manufacturer_vsmart/', ajax_manufacturer_vsmart, name='ajax_manufacturer_vsmart'),
+	path('ajax_manufacturer_xiaomi/', ajax_manufacturer_xiaomi, name='ajax_manufacturer_xiaomi'),
+	path('ajax_manufacturer_realme/', ajax_manufacturer_realme, name='ajax_manufacturer_realme'),
+	path('ajax_manufacturer_masstel/', ajax_manufacturer_masstel, name='ajax_manufacturer_masstel'),
+	path('ajax_manufacturer_huawei/', ajax_manufacturer_huawei, name='ajax_manufacturer_huawei'),
+	path('ajax_manufacturer_nokia/', ajax_manufacturer_nokia, name='ajax_manufacturer_nokia'),
+	
+	path('ajax_manufacturer_totalall/', ajax_manufacturer_totalall, name='ajax_manufacturer_totalall'),
+	path('ajax_manufacturer_total2/', ajax_manufacturer_total2, name='ajax_manufacturer_total2'),
     path('category/<int:id>/<slug:title>', category_products_pro_code, name='category_products_pro_code'),
 
 	path('payment/', include('apps.vnpay_python.urls'), name='payment'),
